c71573885acc3231eaed5e1b76b3d2bf86c79d76
Kiranbadgujar <badgujarkiran07@gmail.com>
2024-08-03 11:22:56 +0530

    add load_manage,qr_code_generator,treeview_structure component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+import RandomColor from './components/random-color';
+import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
+import QRCodeGenerator from "./components/qr-code-generator"
 
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
-      <Accordian/>
+      <Accordian />
+
+      {/* ImageSlider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"5"}
+      />
+
+      {/* LoadMoreData component */}
+      <LoadMoreData />
+
+      {/* RandomColor component */}
+      <RandomColor />
+
+      {/* StarRating component */}
+      <StarRating />
+
+      {/* TreeView component */}
+      <TreeView menus={menus}/>
+
+      {/* QRCodeGenerator component */}
+      <QRCodeGenerator />
+
     </div>
   );
 }
